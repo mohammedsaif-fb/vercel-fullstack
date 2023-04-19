d828138266e0c921123186278927f2d1498f0101
mohammedsaif-fb <89388988+mohammedsaif-fb@users.noreply.github.com>
2023-04-19 20:17:13 +0100

    remove langchain

--- api/index.py
+++ api/index.py
@@ -1,13 +1,15 @@
 import os
-
-from flask import Flask
-from langchain.llms import OpenAI
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    llm = OpenAI(temperature=0.9)
-    text = "What would be a good company name a company that makes colorful socks?"
 
-    return llm(text)
+@app.route('/')
+def home()
+
+
+result_json = {
+    "hi": "hello",
+    "hello": "hi"
+}
+return jsonify(result_json)
